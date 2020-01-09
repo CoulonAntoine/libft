61839b5c03c584d86377b52c758aef05d943abb1
Antoine <contact@cln.ac>
2020-01-09 23:36:00 +0100

    Added ft_lstadd_back.c

---
+++ ft_lstadd_back.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/09 23:08:08 by ancoulon          #+#    #+#             */
+/*   Updated: 2020/01/09 23:35:27 by ancoulon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **alst, t_list *new)
+{
+	if (!alst || !new)
+		return ;
+	if (!(*alst))
+		*alst = new;
+	else
+		ft_lstlast(*alst)->next = new;
+}
