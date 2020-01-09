95c0caf1f2f559c01dc1a1c262f9bc8225558589
Antoine <contact@cln.ac>
2020-01-09 23:51:28 +0100

    Added ft_lstclear.c

---
+++ ft_lstclear.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/09 23:42:09 by ancoulon          #+#    #+#             */
+/*   Updated: 2020/01/09 23:49:13 by ancoulon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*current;
+	t_list	*next;
+
+	if (!lst)
+		return ;
+	current = *lst;
+	while (current)
+	{
+		next = current->next;
+		(*del)(current->content);
+		free(current);
+		current = next;
+	}
+	*lst = NULL;
+}
