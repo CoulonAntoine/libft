a5831c6944044f392b60338aaa91f62dac3c3c7a
Antoine Coulon <ancoulon@student.s19.be>
2020-01-17 07:07:20 +0100

    Updated ft_lstnew.c

--- ft_lstnew.c
+++ ft_lstnew.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstnew.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
+/*   By: ancoulon <ancoulon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/09 22:38:41 by ancoulon          #+#    #+#             */
-/*   Updated: 2020/01/14 06:44:29 by ancoulon         ###   ########.fr       */
+/*   Updated: 2020/01/17 07:05:32 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstnew(void *content)
 {
 	t_list	*el;
 
-	if (!(el = (t_list *)malloc(sizeof(t_list) * 1)))
+	if (!(el = (t_list *)malloc(sizeof(t_list))))
 		return (NULL);
 	el->content = content;
 	el->next = NULL;
