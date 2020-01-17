efdaa949a3f6d32f97facbed5f1c23a66ddeb36d
Antoine Coulon <ancoulon@student.s19.be>
2020-01-17 09:32:08 +0100

    norm

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: ancoulon <ancoulon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/09 22:49:32 by ancoulon          #+#    #+#             */
-/*   Updated: 2020/01/17 09:17:45 by ancoulon         ###   ########.fr       */
+/*   Updated: 2020/01/17 09:24:47 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_lstsize(t_list *lst)
 	size_t	size;
 
 	if (!lst)
-		return (NULL);
+		return (0);
 	size = 0;
 	while (lst)
 	{
