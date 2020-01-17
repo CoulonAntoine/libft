e78ae525ba9ae68abb2b54fcc86956dd9c91af5f
Antoine Coulon <ancoulon@student.s19.be>
2020-01-17 06:47:15 +0100

    Corrected ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ancoulon <ancoulon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/08 10:53:29 by ancoulon          #+#    #+#             */
-/*   Updated: 2020/01/09 06:58:18 by ancoulon         ###   ########.fr       */
+/*   Updated: 2020/01/17 06:41:58 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,11 @@
 
 char		*ft_strjoin(char const *s1, char const *s2)
 {
-	size_t	i;
-	size_t	j;
 	size_t	len;
 	char	*str;
 
 	if (!s1 || !s2)
 		return (NULL);
-	i = 0;
-	j = 0;
 	len = ft_strlen(s1) + ft_strlen(s2) + 1;
 	if (!(str = (char *)malloc(sizeof(char) * len)))
 		return (NULL);
