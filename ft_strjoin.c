9891fa8de236f3a6db0ecb467edc63ce67e6ecfa
Antoine Coulon <ancoulon@student.s19.be>
2020-01-17 16:20:51 +0100

    commit

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,11 +6,27 @@
 /*   By: ancoulon <ancoulon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/08 10:53:29 by ancoulon          #+#    #+#             */
-/*   Updated: 2020/01/17 14:24:59 by ancoulon         ###   ########.fr       */
+/*   Updated: 2020/01/17 15:02:21 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static char	*ft_strcat(char *dest, const char *src)
+{
+	size_t	i;
+	size_t	len;
+
+	i = 0;
+	len = ft_strlen(dest);
+	while (src[i])
+	{
+		dest[len + i] = src[i];
+		i++;
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
 
 char		*ft_strjoin(char const *s1, char const *s2)
 {
@@ -23,7 +39,7 @@ char		*ft_strjoin(char const *s1, char const *s2)
 	if (!(str = (char *)malloc(sizeof(char) * len)))
 		return (NULL);
 	str[0] = '\0';
-	ft_strlcat(str, s1, len - 1);
-	ft_strlcat(str, s2, len);
+	ft_strcat(str, s1);
+	ft_strcat(str, s2);
 	return (str);
 }
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ancoulon <ancoulon@student.s19.be>         +#+  +:+       +#+        */
+/*   By: ancoulon <ancoulon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/09 12:13:22 by ancoulon          #+#    #+#             */
-/*   Updated: 2020/01/09 21:00:37 by ancoulon         ###   ########.fr       */
+/*   Updated: 2020/01/17 16:02:23 by ancoulon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,52 +26,20 @@ static size_t	ft_isset(char c, char const *set)
 	return (0);
 }
 
-static size_t	ft_trimlen(char const *s1, char const *set)
-{
-	size_t		i;
-	size_t		start;
-	size_t		end;
-	size_t		len;
-
-	if (!(len = ft_strlen(s1)))
-		return (0);
-	i = 0;
-	while (s1[i] && ft_isset(s1[i], set))
-		i++;
-	start = i;
-	i = 0;
-	end = len;
-	len--;
-	while (len > 0 && ft_isset(s1[len], set))
-	{
-		i++;
-		len--;
-	}
-	end -= i;
-	return ((long)end - (long)start > 0 ? end - start : 0);
-}
-
 char			*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	i;
-	size_t	j;
 	size_t	len;
-	char	*str;
+	long	start;
+	long	end;
 
 	if (!s1 || !set)
 		return (NULL);
-	len = ft_trimlen(s1, set);
-	i = 0;
-	j = 0;
-	if (!(str = (char *)malloc(sizeof(char) * (len + 1))))
-		return (NULL);
-	while (s1[i] && ft_isset(s1[i], set))
-		i++;
-	while (j < len)
-	{
-		str[j] = s1[i + j];
-		j++;
-	}
-	str[j] = '\0';
-	return (str);
+	start = 0;
+	while (s1[start] && ft_isset(s1[start], set))
+		start++;
+	end = ft_strlen(s1) - 1;
+	while (end >= 0 && ft_isset(s1[end], set))
+		end--;
+	len = (end >= start) ? end - start : 0;
+	return (ft_substr(s1, start, len + 1));
 }
